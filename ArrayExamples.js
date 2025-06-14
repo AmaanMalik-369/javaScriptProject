4cc016468875eaeed2450d5381c7ae4a2b48dbfc
Amaan_Malik <amanmalik12081994@gmail.com>
2025-06-14 18:11:02 +0530

    array example

---
+++ ArrayExamples.js
@@ -0,0 +1,23 @@
+let arrNumbers = [4,7,9,12,16,25]
+
+//print the array
+console.log(arrNumbers)
+
+// print the nth value in the array
+console.log('First value in the array is ' + arrNumbers[0])
+console.log('Second value in the array is ' + arrNumbers[1])
+console.log('Third value in the array is ' + arrNumbers[2])
+console.log('Fourth value in the array is ' + arrNumbers[3])
+console.log('Last value in the array is ' + arrNumbers[5])
+
+arrNumbers = [4,7,9,12,16,25,56,44]
+console.log(arrNumbers)
+console.log('Total number of values are ' + arrNumbers.length)
+console.log('Last value in the array is ' + arrNumbers[arrNumbers.length-1])
+
+let arrNames = ['Mike','Ronny','Julie']
+
+let str1 = 'My name is Nikunj Shah and today is saturday'
+console.log(str1)
+console.log('Total number of words in str1 is ' + str1.split(' ').length)
+
